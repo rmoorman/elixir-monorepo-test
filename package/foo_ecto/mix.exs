395fd984990981dfd86e8dd53e3ef338fba22f71
Rico Moorman <rico.moorman@gmail.com>
2024-03-01 12:55:55 +0100

    Update deps

--- package/foo_ecto/mix.exs
+++ package/foo_ecto/mix.exs
@@ -23,6 +23,8 @@ defmodule FooEcto.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:foo, path: "../foo"},
+      {:hackney, "~> 1.9"},
     ]
   end
 end
