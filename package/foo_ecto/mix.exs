2f457e4e313dc26c88c3b346d8fcd9e0fe5f75ae
Rico Moorman <rico.moorman@gmail.com>
2024-03-01 13:01:59 +0100

    Make dep depending on env

--- package/foo_ecto/mix.exs
+++ package/foo_ecto/mix.exs
@@ -23,8 +23,15 @@ defmodule FooEcto.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:foo, path: "../foo"},
+      foo(),
       {:hackney, "~> 1.9"},
     ]
+  end
+
+  defp foo() do
+    case System.get_env("FOO_DEP") do
+      "LOCAL" -> {:foo, path: "../foo"}
+      _ -> {:foo, git: "https://github.com/rmoorman/elixir-monorepo-test.git", sparse: "package/foo"}
+    end
   end
 end
